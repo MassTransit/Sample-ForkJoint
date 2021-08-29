580dab06d09d01001769fcc515280bf0749c398e
Derek Canham <derek.canham@hotmail.com>
2021-08-30 03:49:53 +0800

    created new projects

---
+++ src/ForkJoint.Service/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.Hosting;
+
+namespace ForkJoint.Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    
+                });
+    }
+}
